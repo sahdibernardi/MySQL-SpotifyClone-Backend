1f418c41080bc7c97046db4c9359d1a587650c1b
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-02-16 01:39:06 -0300

    remove: comments

--- desafio5.sql
+++ desafio5.sql
@@ -4,14 +4,9 @@ SELECT
 
 FROM
     SpotifyClone.songs AS s
--- tabelas usadas: songs, reproductions
     INNER JOIN SpotifyClone.reproductions AS r
         ON r.song_id = s.song_id
-
--- ordenado em ordem decrescente, baseando-se no número de reproduções. Em caso de empate, ordene os resultados pelo nome da canção em ordem alfabética.
 GROUP BY song
 ORDER BY
     reproducoes DESC,  cancao ASC
-
--- Queremos apenas o top 2 de músicas mais tocadas.
 LIMIT 2;
